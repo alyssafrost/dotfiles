d11ff1d2438bf1e06ab8e8604faaa84d175a85d7
Alyssa Frost <frost7@uab.edu>
2021-08-10 22:24:04 -0500

    Add autojump

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
 
 # Created by `pipx` on 2021-07-03 18:53:39
 set PATH $PATH /home/frost7/.local/bin
+
+# autojump
+[ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
+
