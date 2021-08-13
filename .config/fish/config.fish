e2fb06330bf8453099061c6886077c22ff9140f6
Alyssa Frost <frost7@uab.edu>
2021-08-13 13:41:37 -0500

    More fish, aur, and pacman things

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,6 @@ set PATH $PATH /home/frost7/.local/bin
 # autojump
 [ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
 
+
+# Created by `pipx` on 2021-08-13 14:35:01
+set PATH $PATH /home/svving/.local/bin
